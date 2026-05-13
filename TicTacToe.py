62a6aad38146713b310368b125995a0630a1902f
VitSikora <sikora.v.26@zszatopkovych.eu>
2026-05-13 14:15:47 +0200

    tictac

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,5 +1,5 @@
-#Podle tutorialu https://www.youtube.com/watch?v=nbRpDXV7QDM
-import tkinter as tk
+#tutorial Kenny Yip Coding
+import tkinter 
 
 def set_tile(row, column):
     global curr_player
@@ -8,44 +8,84 @@ def set_tile(row, column):
         return
 
     if board[row][column]["text"] != "":
+        #already taken spot
         return
     
-    board[row][column]["text"] = curr_player
-    
-    if curr_player == playerO:
+    board[row][column]["text"] = curr_player 
+
+    if curr_player == playerO: 
         curr_player = playerX
     else:
         curr_player = playerO
     
     label["text"] = curr_player+"'s turn"
 
+    #check winner
     check_winner()
 
 def check_winner():
     global turns, game_over
     turns += 1
 
+    #horizontally, check 3 rows
     for row in range(3):
-        if (board[row][0]["text"] == board[row][1]["text"] == board[row][2]["text"] 
+        if (board[row][0]["text"] == board[row][1]["text"] == board[row][2]["text"]
             and board[row][0]["text"] != ""):
-            label.config(text=board[row][0]["text"]+" is the winner", foreground=color_yellow)
-            for column in range (3):
-                board[row][column].config(foreground=color_yellow)
+            label.config(text=board[row][0]["text"]+" is the winner!", foreground=color_yellow)
+            for column in range(3):
+                board[row][column].config(foreground=color_yellow, background=color_white)
             game_over = True
             return
     
-    for column in range (3):
-        if (board[0][column]["text"] == board [1][column]["text"] ==board[2][column]["text"]
-            and board [0][column]["text"] != ""):
-            label.config(text=board[0][column]["text"]+" is the winner, foreground=color_yellow")
-            for column in range (3):
-                board[row][column].config(foreground=color_yellow)
+    #vertically, check 3 columns
+    for column in range(3):
+        if (board[0][column]["text"] == board[1][column]["text"] == board[2][column]["text"]
+            and board[0][column]["text"] != ""):
+            label.config(text=board[0][column]["text"]+" is the winner!", foreground=color_yellow)
+            for row in range(3):
+                board[row][column].config(foreground=color_yellow, background=color_white)
             game_over = True
             return
+    
+    #diagonally
+    if (board[0][0]["text"] == board[1][1]["text"] == board[2][2]["text"]
+        and board[0][0]["text"] != ""):
+        label.config(text=board[0][0]["text"]+" is the winner!", foreground=color_yellow)
+        for i in range(3):
+            board[i][i].config(foreground=color_yellow, background=color_white)
+        game_over = True
+        return
+
+    #anti-diagionally
+    if (board[0][2]["text"] == board[1][1]["text"] == board[2][0]["text"]
+        and board[0][2]["text"] != ""):
+        label.config(text=board[0][2]["text"]+" is the winner!", foreground=color_yellow)
+        board[0][2].config(foreground=color_yellow, background=color_white)
+        board[1][1].config(foreground=color_yellow, background=color_white)
+        board[2][0].config(foreground=color_yellow, background=color_white)
+        game_over = True
+        return
+    
+    #tie
+    if (turns == 9):
+        game_over = True
+        label.config(text="Tie!", foreground=color_yellow)
+
 
 def new_game():
-    pass
-    
+    global turns, game_over
+
+    turns = 0
+    game_over = False
+
+    label.config(text=curr_player+"'s turn", foreground="white")
+
+    for row in range(3):
+        for column in range(3):
+            board[row][column].config(text="", foreground=color_light_blue , background=color_blue)
+
+
+#game setup
 playerX = "X"
 playerO = "O"
 curr_player = playerX
@@ -53,47 +93,48 @@ board = [[0, 0, 0],
          [0, 0, 0], 
          [0, 0, 0]]
 
-color_black = "#000000"
-color_dark_blue = "#123CF8"
-color_yellow = "#DAFD15"
-color_light_blue = "#ADD8E6"
+color_light_blue = "#76c1ff"
+color_yellow = "#f1da7d"
+color_blue = "#343396"
+color_white = "#FCF5F5"
 
 turns = 0
 game_over = False
 
-#window
-window = tk.Tk()
+#window setup
+window = tkinter.Tk() 
 window.title("Tic Tac Toe")
 window.resizable(False, False)
 
-frame = tk.Frame(window)
-label = tk.Label(frame, text=curr_player+"'s turn", font=("Calibri", 20), background=color_black, foreground="white")
-
+frame = tkinter.Frame(window)
+label = tkinter.Label(frame, text=curr_player+"'s turn", font=("Consolas", 20), background=color_blue,
+                      foreground="white")
 label.grid(row=0, column=0, columnspan=3, sticky="we")
 
 for row in range(3):
     for column in range(3):
-        board[row][column] = tk.Button(frame, text="", font=("Calibri", 50, "bold"), background=color_black, foreground=color_dark_blue, width=4, height=1, command=lambda row=row, column=column: set_tile(row, column))
-        board[row][column].grid(row=row+1,column=column)
+        board[row][column] = tkinter.Button(frame, text="", font=("Consolas", 50, "bold"),
+                                            background=color_blue, foreground=color_light_blue , width=4, height=1,
+                                            command=lambda row=row, column=column: set_tile(row, column))
+        board[row][column].grid(row=row+1, column=column)
 
-button = tk.Button(frame, text="restart", font=("Calibri", 20), background=color_black, foreground="white", command=new_game)
-button.grid(row=4, column=0, columnspan = 3, sticky="we")
+button = tkinter.Button(frame, text="restart", font=("Consolas", 20), background=color_blue,
+                        foreground="white", command=new_game)
+button.grid(row=4, column=0, columnspan=3, sticky="we")
 
 frame.pack()
 
+#center the window
 window.update()
 window_width = window.winfo_width()
 window_height = window.winfo_height()
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
 
-
 window_x = int((screen_width/2) - (window_width/2))
 window_y = int((screen_height/2) - (window_height/2))
 
-"format (h)x(w)+(x)+(y)"
-
+#format "(w)x(h)+(x)+(y)"
 window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
-
 
 window.mainloop()
